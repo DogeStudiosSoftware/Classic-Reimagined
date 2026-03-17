fba3f98f0a830aa7b2551f18b91744ea5baa4fd0
MrGlitchDogePE <211275175+MrGlitchDogePE@users.noreply.github.com>
2026-03-16 20:59:38 -0600

    Update fog_classic.glsl

--- 1.21.6_environmental_fog/assets/minecraft/shaders/include/fog_classic.glsl
+++ 1.21.6_environmental_fog/assets/minecraft/shaders/include/fog_classic.glsl
@@ -15,16 +15,15 @@ float linear_fog_value(float vertexDistance, float fogStart, float fogEnd) {
 }
 
 float total_fog_value(float sphericalVertexDistance, float cylindricalVertexDistance, float environmentalStart, float environmentalEnd, float renderDistanceStart, float renderDistanceEnd) {
-    return mix(max(
-        linear_fog_value(cylindricalVertexDistance, environmentalStart, environmentalEnd),
+    return max(clamp(
+        linear_fog_value(sphericalVertexDistance, min(renderDistanceEnd, environmentalEnd) / 4, min(renderDistanceEnd, environmentalEnd)),
         pow(linear_fog_value(sphericalVertexDistance, 0, 1024),
         (1.0f - linear_fog_value(sphericalVertexDistance, min(8, min(renderDistanceEnd, environmentalEnd) / 4), min(renderDistanceEnd, environmentalEnd))) * 1.25 *
-        (min(8, min(renderDistanceEnd, environmentalEnd) / 4) / 8))
-    ),
-        linear_fog_value(sphericalVertexDistance, min(renderDistanceEnd, environmentalEnd) / 4, min(renderDistanceEnd, environmentalEnd)),
-        linear_fog_value(sphericalVertexDistance, 0, min(renderDistanceEnd, environmentalEnd))
+        (min(8, min(renderDistanceEnd, environmentalEnd) / 4) / 8)),
+        linear_fog_value(sphericalVertexDistance, min(renderDistanceEnd, environmentalEnd) / 4, min(renderDistanceEnd, environmentalEnd))),
+        linear_fog_value(cylindricalVertexDistance, environmentalStart, environmentalEnd)
     );
-}
+};
 
 vec4 apply_fog(vec4 inColor, float sphericalVertexDistance, float cylindricalVertexDistance, float environmentalStart, float environmentalEnd, float renderDistanceStart, float renderDistanceEnd, vec4 fogColor) {
     float fogValue = total_fog_value(sphericalVertexDistance, cylindricalVertexDistance, environmentalStart, environmentalEnd, renderDistanceStart, renderDistanceEnd);
